1aa24ffb500996fe1f921817aff62cf8a4542e18
Benoît Baliguet <benoit.baliguet@gmail.com>
2015-11-19 18:33:56 +0100

    WIP on build, readjust for eslint

--- webpack.config.js
+++ webpack.config.js
@@ -2,36 +2,27 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'source-map',
-  resolve: {
-    root: __dirname + '/source'
-  },
-  entry: [
-    './source/client/index'
-  ],
-  output: {
-    path: path.join(__dirname, 'build'),
-    filename: 'index.js',
-    publicPath: '/static/'
-  },
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false
-      }
-    })
-  ],
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'source')
-    }]
-  }
+	devtool: 'source-map',
+	resolve: {
+		root: __dirname + '/source'
+	},
+	entry: './source/roadmap.js',
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'roadmap.js'
+	},
+	plugins: [
+		new webpack.optimize.UglifyJsPlugin({
+			compressor: {
+				warnings: false
+			}
+		})
+	],
+	module: {
+		loaders: [{
+			test: /\.js$/,
+			loader: 'babel-loader',
+			include: path.join(__dirname, 'source')
+		}]
+	}
 };
